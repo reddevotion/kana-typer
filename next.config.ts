54f774ad202b749474d47098e6496338aa1de527
Valery Reddevotion <linkolnsid228@gmail.com>
2025-06-12 14:59:33 +0700

    update package.json build script

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/kana-typer",
+  assetPrefix: "/kana-typer",
 };
 
 export default nextConfig;
