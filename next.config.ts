3030ef0b776d19c7d0c2196ca2daec8d703d5c8c
Valery Reddevotion <linkolnsid228@gmail.com>
2025-06-12 15:03:43 +0700

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/kana-typer",
-  assetPrefix: "/kana-typer",
+  /* config options here */
 };
 
 export default nextConfig;
